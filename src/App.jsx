ec5368cb7596688f90cd2ef08b2707c244092cd5
Markus Dybedal <markus4356@gmail.com>
2022-09-15 16:07:25 +0200

    feat: views and paths to them added

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,28 @@
-import logo from './logo.svg';
 import './App.css';
+import {
+  BrowserRouter,
+  Routes,
+  Route
+} from 'react-router-dom';
+import Startup from './views/Startup';
+import Profile from './views/Profile';
+import Translation from './views/Translation';
+
 
 function App() {
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <div className="App">
+        <Routes>
+          <Route>
+            <Route path="/" element={<Startup />} />
+            <Route path="/profile" element={<Profile />} />
+            <Route path="/translation" element={<Translation />} />
+          </Route>
+        </Routes>
+      </div>
+    </BrowserRouter>
   );
 }
 
