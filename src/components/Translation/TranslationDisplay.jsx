8738241f228d4ea7bc44b238efc623ebd48bed86
Markus Dybedal <markus4356@gmail.com>
2022-09-20 13:36:13 +0200

    feat: split string into array of chars

--- src/components/Translation/TranslationDisplay.jsx
+++ src/components/Translation/TranslationDisplay.jsx
@@ -1,11 +1,15 @@
-const TranslationDisplay = ({character}) => {
-    return(
-        <>
-        
-            <img src = {`../pictures/individual_signs${character}.png`} alt={character}/> 
-        
-        </>
-    )
-}
+const TranslationDisplay = ({translationText}) => {
 
+    const letterArray = translationText.split("")
+
+    return (
+        <>
+            {letterArray.map((character) =>(
+              <img src = {`../pictures/individual_signs${character}.png`}
+              alt={character}/>
+            ))}
+        </>
+      );
+    };
+    
 export default TranslationDisplay
