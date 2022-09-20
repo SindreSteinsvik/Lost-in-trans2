66de23a644d4c323f45d4a1484614c0561960510
SindreSteinsvik <STorne@N-NO-01-03-3970>
2022-09-20 20:42:56 +0200

    feat: removed app.js

--- src/App.js
+++ src/App.js
@@ -1,1 +1,35 @@
-const port = process.env.PORT || 3000
+import './App.css';
+import {
+  BrowserRouter,
+  Routes,
+  Route
+} from 'react-router-dom';
+import Startup from './views/Startup';
+import Profile from './views/Profile';
+import Translation from './views/Translation';
+import Navbar from './components/Navbar/Navbar';
+
+
+function App() {
+
+
+
+  
+  return (
+    <BrowserRouter>
+      <div className="App">
+        <Navbar />
+    
+        <Routes>
+          <Route>
+            <Route path="/" element={<Startup />} />
+            <Route path="/profile" element={<Profile />} />
+            <Route path="/translation" element={<Translation />} />
+          </Route>
+        </Routes>
+      </div>
+    </BrowserRouter>
+  );
+}
+
+export default App;
